96352fc6e807de691f626639098c0b8a309b4472
i-novillo <89866823+i-novillo@users.noreply.github.com>
2025-03-06 20:58:22 +0100

    Add ROS2 Launch File (#12)
    - Create Launch file
    - Fix I2C message parser

---
+++ raspberry_pi/ros2_central_ws/launch/rover_launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='i2c_manager',
+            executable='i2c_manager',
+            name='i2c_manager'
+        ),
+        Node(
+            package='motor_manager',
+            executable='motor_manager',
+            name='motor_manager'
+        )
+    ])
