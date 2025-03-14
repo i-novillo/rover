5767e383ff802dadc42174a4e197b129bcee2a4c
Ignacio Novillo <ignovillo1@gmail.com>
2025-03-14 12:16:57 +0100

    - Change BT server to launch from Raspi instead of windows

--- raspberry_pi/ros2_central_ws/README.md
+++ raspberry_pi/ros2_central_ws/README.md
@@ -1,2 +1,4 @@
 ## IMPORTANT
 * For the pigpio module to work, the daemon needs to be running in an IPv4 interface not IPv6 ("You can check where it is listening, by starting pigpiod and then running "netstat -vatn" in a terminal."). For that, when starting the daemon do `sudo pigpiod -n 127.0.0.1`
+
+* If bluetooth is not working, it's possible the BT interface needs to be set to discoverable. For that, do `bluetoothctl` and then `discoverable on`
--- raspberry_pi/ros2_central_ws/src/bt_comms/bt_comms/ble_test.py
+++ raspberry_pi/ros2_central_ws/src/bt_comms/bt_comms/ble_test.py
@@ -1,22 +1,31 @@
 import socket
 
-client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-client.connect(("00:1A:7D:DA:71:15", 4))
+# Steps
+# 1. Device Manager -> Realtek Bluetooth Adapter
+# 2. Right Click -> Properties -> Advanced -> Address
+# 3. Turn on Bluetooth on both devices and make server device visible
 
-print(f"Connected!")
+server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)  # RFCOMM specific protocol
+server.bind(("B8:27:EB:04:61:FE", 4))  # MAC Address and Channel 4
+server.listen(1)
+
+print("Waiting for connection...")
+
+client, addr = server.accept()
+print(f"Accepted connection from {addr}")
 
 try:
     while True:
-        message = input("Enter message: ")
-        client.send(message.encode('utf-8'))
         data = client.recv(1024)
         if not data:
             break
         print(f"Received: {data.decode('utf-8')}")
-
+        message = input("Enter message: ")
+        client.send(message.encode('utf-8'))
 except OSError:
     pass
 
 print("Disconnected")
 
 client.close()
+server.close()
