fd0d6a39c6a254a576caf5092dcb1dbf0d49bdc1
Ignacio Novillo <ignovillo1@gmail.com>
2025-04-09 16:06:11 +0200

    - Added BT message standard to BT client

--- GUI/bt_client.py
+++ GUI/bt_client.py
@@ -9,9 +9,19 @@ class BTClient:
 
     def send_move_cmd(self, cmd):
         try:
-            self.client.send(cmd.encode('utf-8'))
+            msg = self.parse_bt_msg(0x01, cmd)
+            self.client.send(msg)
         except OSError:
             print("Error sending forward command")
+
+    def parse_bt_msg(self, id, msg):
+        msg_bytes = msg.encode('utf-8')
+        msg_length = len(msg_bytes)
+        parsed_msg = bytearray()
+        parsed_msg.append(id)
+        parsed_msg.append(msg_length)
+        parsed_msg.extend(msg_bytes)
+        return parsed_msg
 
     def close_connection(self):
         self.client.close()
